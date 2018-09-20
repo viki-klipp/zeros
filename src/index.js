131fc86aed6ef3cb0ab28444034a1b05d1cd9e9c
viki-klipp <viki.klipp@gmail.com>
2018-09-20 16:33:28 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let multiplier = 5,
+    arr = [],
+    result = 0;
+
+  while(arr[arr.length-1] >= 1 || arr.length == 0) {
+    arr.push(Math.floor(number / multiplier));
+    multiplier *= 5;
+  }
+  
+  arr.forEach((el) => result += el);
+
+  return result;
 }
